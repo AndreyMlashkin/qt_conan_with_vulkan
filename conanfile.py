137f41b4d18aa580e118da8a1858f144482a9b94
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-09-01 15:31:40 +0200

    cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,34 +3,10 @@ from conans.model.version import Version
 import subprocess
 import os, getpass, string, unicodedata
 
-url = "git@gitlab.com:reactivereality/contentpipeline/pictofitstudio.git"
-
-def cleanName(name):
-    validUsernameChars = "_{}{}".format(string.ascii_letters, string.digits)
-    replaceChars = " \t"
-    for c in replaceChars:
-        username = name.replace(c,'_')
-
-    # keep only valid ascii chars
-    cleanedUsername = unicodedata.normalize('NFKD', username).encode('ASCII', 'ignore').decode()
-
-    # keep only whitelisted chars
-    cleanedUsername = ''.join(c for c in cleanedUsername if c in validUsernameChars)
-    return cleanedUsername
-
-def get_user():
-    username = cleanName(getpass.getuser())
-    return username if username else "user"
-
 class PictofitStudioConan(ConanFile):
-    name = "pictofitstudio"
-    default_user    = get_user()
-    default_channel = "testing"
-    url             = url
-    keep_imports = True
-    topics = ("gui", "image_processing", "reactive_reality")
+    name = "example"
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "*", "!pictofitstudio-gui-tests/*", "pictofitstudio/libraries/*"
+    exports_sources = "*"
     options = {"shared"                            : [True, False],
                "fPIC"                              : [True, False]
               }
@@ -38,10 +14,6 @@ class PictofitStudioConan(ConanFile):
                        "fPIC"   : True
                        }
     generators = "qt", "cmake", "cmake_find_package_multi", "qmake"
-
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        self.version = git.run("describe --always --tags --abbrev=9")
 
     def package_id(self):
         if self.settings.compiler == "Visual Studio":
@@ -51,9 +23,6 @@ class PictofitStudioConan(ConanFile):
                 self.info.settings.compiler.runtime = "MT/MTd"
 
     def requirements(self):
-        if conan_version < Version("1.28"):
-            raise ConanInvalidConfiguration("update conan client to version >= 1.28")
-
         self.requires.add('qt/6.1.2')
         self.options["qt"].shared = True
         self.options["qt"].with_vulkan = True
